c546a2900f357ac782cdbb7506f8a1417aa29c61
anasousa123 <ana.7410062@aluno.mg.gov.br>
2026-04-28 07:44:18 -0300

    ol

--- app.js
+++
@@ -1,6 +0,0 @@
-let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
-
-function add(nome, preco){
-  carrinho.push({nome, preco});
-  localStorage.setItem("carrinho", JSON.stringify(carrinho));
-  alert("Adicionado!");
