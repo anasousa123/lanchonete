d34210125b4f2ea15c9f1d7f67546365c10c7f7d
anasousa123 <ana.7410062@aluno.mg.gov.br>
2026-04-28 21:03:31 -0300

    hjn

--- app.js
+++ app.js
@@ -1,49 +1,25 @@
 let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
 
-// PRODUTOS EXEMPLO
-const produtos = {
-  lanches: [
-    {nome:"X-Burger", preco:20, img:"https://images.unsplash.com/photo-1550547660-d9450f859349"},
-    {nome:"X-Salada", preco:22, img:"https://images.unsplash.com/photo-1568901346375-23c9450c58cd"}
-  ],
-  bebidas: [
-    {nome:"Refrigerante", preco:8, img:"https://images.unsplash.com/photo-1581009137042-c552e485697a"}
-  ],
-  porcoes: [
-    {nome:"Batata Frita", preco:15, img:"https://images.unsplash.com/photo-1586190848861-99aa4a171e90"}
-  ]
-};
-
-function carregarProdutos(){
-
-  let categoria = localStorage.getItem("categoria");
-  let lista = document.getElementById("lista");
-  let titulo = document.getElementById("titulo");
-
-  if(!lista) return;
-
-  titulo.innerText = categoria.toUpperCase();
-
-  let html = "";
-
-  produtos[categoria].forEach(p=>{
-    html += `
-      <div class="card">
-        <img src="${p.img}">
-        <h3>${p.nome}</h3>
-        <p>R$ ${p.preco}</p>
-        <button onclick="add('${p.nome}', ${p.preco})">Adicionar</button>
-      </div>
-    `;
-  });
-
-  lista.innerHTML = html;
-}
-
 function add(nome, preco){
   carrinho.push({nome, preco});
   localStorage.setItem("carrinho", JSON.stringify(carrinho));
   alert("Adicionado!");
 }
 
-carregarProdutos();
+if(document.getElementById("lista")){
+  let lista = document.getElementById("lista");
+  let total = 0;
+
+  carrinho.forEach(i=>{
+    lista.innerHTML += `<p>${i.nome} - R$ ${i.preco}</p>`;
+    total += i.preco;
+  });
+
+  document.getElementById("total").innerText = "Total: R$ " + total;
+}
+
+function finalizar(){
+  localStorage.removeItem("carrinho");
+  alert("Pedido enviado!");
+  location.href="status.html";
+}
